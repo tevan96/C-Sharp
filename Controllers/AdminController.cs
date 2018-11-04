a04d0483c0c6eee8eb30ace1ef1ebff0989b1d5f
tevan96 <44743732+tevan96@users.noreply.github.com>
2018-11-04 17:35:59 +0800

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GussmannTechnologyWebsite.Models;
+
+using System.Web.Mvc;
+using System.IO;
+
+namespace GussmannTechnologyWebsite.Controllers
+{
+    public class AdminController : Controller
+    {
+        GussmannPhilippinesDBEntities db = new GussmannPhilippinesDBEntities();
+        // GET: Admin
+        public ActionResult AdminIndex()
+        {
+            var image = db.HomeTbls.ToList();
+
+            ViewBag.Image = image;
+
+            return View();
+        }
+
+        public ActionResult AddImageHome()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddImageHome(HomeTbl hmtbl)
+        {
+            string filename = Path.GetFileNameWithoutExtension(hmtbl.ImageFile.FileName);
+            string extension = Path.GetExtension(hmtbl.ImageFile.FileName);
+            filename = DateTime.Now.ToString("yymmssfff") + extension;
+            hmtbl.ImagePath = "~/Images/Home/" + filename;
+            filename = Path.Combine(Server.MapPath("~/Images/Home/"), filename);
+            hmtbl.ImageFile.SaveAs(filename);
+
+            db.HomeTbls.Add(hmtbl);
+            db.SaveChanges();
+            return RedirectToAction("AdminIndex");
+
+        }
+
+        public ActionResult DeleteImageHome(int id)
+        {
+            var HMID = db.HomeTbls.Find(id);
+            string fullPath = Request.MapPath(HMID.ImagePath);
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+            }
+            db.HomeTbls.Remove(HMID);
+            db.SaveChanges();
+
+
+            return RedirectToAction("AdminIndex");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GussmannTechnologyWebsite.Models;
+
+using System.Web.Mvc;
+using System.IO;
+
+namespace GussmannTechnologyWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        GussmannPhilippinesDBEntities db = new GussmannPhilippinesDBEntities();
+        public ActionResult Index()
+        {
+            var image = db.HomeTbls.ToList();
+
+            ViewBag.Image = image;
+
+            return View();
+        }
+
+
+    
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
